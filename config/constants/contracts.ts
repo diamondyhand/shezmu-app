0944c34fb934cf6b60c84d9278d50cd340ef01b9
yeshealer <yeshealer@gmail.com>
2023-09-05 21:57:08 +0900

    update mainnet contract addresses

--- config/constants/contracts.ts
+++ config/constants/contracts.ts
@@ -1,14 +1,14 @@
 export default {
   tokenSale: {
     5: "0x0Eb9BD185d0a15a0b4e1dD2C8fb89B60225FbfaA",
-    1: "0x0Eb9BD185d0a15a0b4e1dD2C8fb89B60225FbfaA",
+    1: "0x2DCdd9ccBdA7BFe4ED22a6e8A2424eFb06d5Fbe8",
   },
   shezmu: {
     5: "0x90ABd0694d7b75Fc588dc5b6e1151B1Bb94B32E6",
-    1: "0x90ABd0694d7b75Fc588dc5b6e1151B1Bb94B32E6",
+    1: "0x5fE72ed557d8a02FFf49B3B826792c765d5cE162",
   },
   guardian: {
     5: "0xbf02324b700c7A93CEbBf1854559CcA322D5587C",
-    1: "0xbf02324b700c7A93CEbBf1854559CcA322D5587C",
+    1: "0x33972a4cf6c4c832ee4fca99a5cfA046758Fca35",
   },
 };
