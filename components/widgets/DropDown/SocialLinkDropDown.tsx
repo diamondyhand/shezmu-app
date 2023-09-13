948e814f78bec5b53580c4ec61713f16eedbc2fc
diamondyhand <masterdevv278@gmail.com>
2023-09-13 05:15:14 +0200

    feat: socialLink dropdown in the header.

---
+++ components/widgets/DropDown/SocialLinkDropDown.tsx
@@ -0,0 +1,79 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+
+export default function SocialLinkDropDown() {
+  return (
+    <>
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-white">
+          Socials
+          <ChevronDownIcon className="-mr-1 h-5 w-5 text-white" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-24 origin-top-right rounded-md bg-[#18181B] border border-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">           
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="https://t.me/shezmueth"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-white',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Telegram
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="https://twitter.com/ShezmuTech"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-white',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Twitter
+                </a>
+              )}
+            </Menu.Item>
+            <form method="POST" action="#">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href='https://shezmu.gitbook.io/shezmu/introduction/welcome-to-shezmu'
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-white',
+                      'block w-full px-4 py-2 text-left text-sm'
+                    )}
+                  >
+                    Gitbook
+                  </a>
+                )}
+              </Menu.Item>
+            </form>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+    </>
+  )
+}
--- components/widgets/Header/index.tsx
+++ components/widgets/Header/index.tsx
@@ -17,79 +17,10 @@ import { IndexRouterLinkConfig, DappRouterLinkConfig } from '../Constants/main';
 import { useNetwork } from 'wagmi';
 import { getWalletClient } from '@/utils/viemHelper';
 import { CHAIN_ID } from '@/config/constants/network';
+import SocialLinkDropDown from '../DropDown/SocialLinkDropDown';
 
 const manrope = Manrope({ subsets: ['latin'] })
 
-function classNames(...classes: any) {
-    return classes.filter(Boolean).join(" ");
-}
-  
-const SocialLink = () => {
-    const [isDropDown, setIsDropDown] = useState(false);
-    return (
-        <>
-        <div className="inline-flex">
-            <a
-                onClick={() => setIsDropDown(!isDropDown)}
-                className="py-2 text-sm text-white font-bold cursor-pointer"
-            >
-                Socials
-            </a>
-
-            <div className="relative">
-                <button
-                    type="button"
-                    className="inline-flex items-center justify-center h-full px-2 text-white hover:text-white-700"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-4 h-4"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M19 9l-7 7-7-7"
-                        />
-                    </svg>
-                </button>
-
-                {/* <div className='absolute right-0 z-10 mt-3 origin-top-right bg-white border border-gray-100 rounded-md shadow-lg '> */}
-                <div className={classNames(
-                                isDropDown
-                                  ? "hidden"
-                                  : "block",
-                                "absolute right-0 z-10 mt-3 origin-top-right bg-[#18181B] border border-gray-100 rounded-md shadow-lg"
-                              )}>
-                    <div className="p-2">
-                        <a
-                            href="https://t.me/shezmueth"
-                            className="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700"
-                        >
-                            Telegram
-                        </a>
-                        <a
-                            href="https://twitter.com/ShezmuTech"
-                            className="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700"
-                        >
-                            Twitter
-                        </a>
-                        <a
-                            href="https://shezmu.gitbook.io/shezmu/introduction/welcome-to-shezmu"
-                            className="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700"
-                        >
-                            Gitbook
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </>
-    )
-}
 export default function Header() {
     const [RouterLinkConfig, setRouterLinkConfig] = useState(IndexRouterLinkConfig);
     const [isDapp, setIsDapp] = useState(false);
@@ -151,7 +82,7 @@ export default function Header() {
                             </Link>
                         ))}
                         {
-                            isDapp == false ? <SocialLink /> : <></>
+                            isDapp == false ? <SocialLinkDropDown /> : <></>
                         }
                     </div>
                     <ConnectWalletBtn />
