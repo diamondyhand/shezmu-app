e4a59921441cb356185be31cad8d9eaa3396903f
diamondyhand <masterdevv278@gmail.com>
2023-09-18 16:47:25 +0300

    fix: updated back image.

--- components/views/Landingpage/ImageSection.tsx
+++ components/views/Landingpage/ImageSection.tsx
@@ -1,8 +1,8 @@
 export default function ImageSection() {
     return (
         <div className='w-full flex justify-center bg-[#050508] mt-0 md:-mt-16 px-5 md:px-12 lg:px-16 xl:px-0'>
-            <img src='/image/landing_img_1.png' alt='placeholder' draggable={false} className="hidden md:block" />
-            <img src='/image/landing_img_2.png' alt='placeholder' draggable={false} className="block md:hidden w-full" />
+            <img src='/image/landing_image_1.png' alt='placeholder' draggable={false} className="hidden md:block" />
+            <img src='/image/landing_image_2.png' alt='placeholder' draggable={false} className="block md:hidden w-full" />
         </div>
     )
 }
